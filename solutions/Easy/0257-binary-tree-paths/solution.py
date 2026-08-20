191d51e093fb09d9ad9cc1ba836154e1c89e0138
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-20 05:33:41 +0000

    leetie: Add 257. Binary Tree Paths [Easy] (python)

--- solutions/Easy/0257-binary-tree-paths/solution.py
+++ solutions/Easy/0257-binary-tree-paths/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : String, Backtracking, Tree, Depth-First Search, Binary Tree
 # Link     : https://leetcode.com/problems/binary-tree-paths/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12336000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12584000 (beats 16%)
 # Language : python
 # Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 # Synced by: leetie
