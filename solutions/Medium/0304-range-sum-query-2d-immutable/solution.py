4eabf55734e4d7a55a98f141a527c6f1c8118b7f
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-21 04:21:23 +0000

    leetie: Add 304. Range Sum Query 2D - Immutable [Medium] (python)

--- solutions/Medium/0304-range-sum-query-2d-immutable/solution.py
+++ solutions/Medium/0304-range-sum-query-2d-immutable/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Design, Matrix, Prefix Sum
 # Link     : https://leetcode.com/problems/range-sum-query-2d-immutable/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12420000 (beats 0%)
+# Runtime  : 180 ms (beats 58%)
+# Memory   : 24644000 (beats 59%)
 # Language : python
 # Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 # Synced by: leetie
