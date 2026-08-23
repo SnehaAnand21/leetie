6e9139f0fb0fe43541aab13db7bd054d27723a16
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-23 16:31:52 +0000

    leetie: Add 196. Delete Duplicate Emails [Easy] (mysql)

--- solutions/Easy/0196-delete-duplicate-emails/solution.sql
+++ solutions/Easy/0196-delete-duplicate-emails/solution.sql
@@ -3,7 +3,7 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/delete-duplicate-emails/
--- Runtime  : 1237 ms (beats 7%)
+-- Runtime  : 1035 ms (beats 23%)
 -- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 SnehaAnand21. All rights reserved.
