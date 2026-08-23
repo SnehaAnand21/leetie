6c0f25042fddbdc27a37a3ef331a3d189e6e0959
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-23 16:09:51 +0000

    leetie: Add 386. Lexicographical Numbers [Medium] (java)

--- solutions/Medium/0386-lexicographical-numbers/solution.java
+++ solutions/Medium/0386-lexicographical-numbers/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Depth-First Search, Trie
 // Link     : https://leetcode.com/problems/lexicographical-numbers/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42364000 (beats 0%)
+// Runtime  : 2 ms (beats 87%)
+// Memory   : 50248000 (beats 36%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
