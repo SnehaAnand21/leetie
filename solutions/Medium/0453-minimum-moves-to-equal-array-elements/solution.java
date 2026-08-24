8a06c374547ff0e875135da39e6969e85bf2b019
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-24 06:14:26 +0000

    leetie: Add 453. Minimum Moves to Equal Array Elements [Medium] (java)

--- solutions/Medium/0453-minimum-moves-to-equal-array-elements/solution.java
+++ solutions/Medium/0453-minimum-moves-to-equal-array-elements/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Math
 // Link     : https://leetcode.com/problems/minimum-moves-to-equal-array-elements/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42656000 (beats 0%)
+// Runtime  : 2 ms (beats 92%)
+// Memory   : 47152000 (beats 67%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
