1c1ed224891fdfa1c4ee0181319fe1eaf34f9715
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-24 03:55:11 +0000

    leetie: Add 447. Number of Boomerangs [Medium] (java)

--- solutions/Medium/0447-number-of-boomerangs/solution.java
+++ solutions/Medium/0447-number-of-boomerangs/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Hash Table, Math
 // Link     : https://leetcode.com/problems/number-of-boomerangs/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42464000 (beats 0%)
+// Runtime  : 144 ms (beats 20%)
+// Memory   : 46916000 (beats 37%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
