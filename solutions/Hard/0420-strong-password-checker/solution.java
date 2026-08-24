0f8acc316e34b73e5d0f0faa9894d6ae116e5b88
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-24 03:40:15 +0000

    leetie: Add 420. Strong Password Checker [Hard] (java)

--- solutions/Hard/0420-strong-password-checker/solution.java
+++ solutions/Hard/0420-strong-password-checker/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : String, Greedy, Heap (Priority Queue)
 // Link     : https://leetcode.com/problems/strong-password-checker/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42568000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 42816000 (beats 25%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
