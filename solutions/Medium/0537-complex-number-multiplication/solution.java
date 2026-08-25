d5fdbeb447e297a7c833275ae39f895af45510ef
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-25 09:24:55 +0000

    leetie: Add 537. Complex Number Multiplication [Medium] (java)

--- solutions/Medium/0537-complex-number-multiplication/solution.java
+++ solutions/Medium/0537-complex-number-multiplication/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, String, Simulation
 // Link     : https://leetcode.com/problems/complex-number-multiplication/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42956000 (beats 0%)
+// Runtime  : 1 ms (beats 97%)
+// Memory   : 42444000 (beats 95%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
