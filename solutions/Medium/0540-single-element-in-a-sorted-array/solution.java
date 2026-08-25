4c282b821a171f950680168962e6bcdcb3c33439
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-25 09:34:05 +0000

    leetie: Add 540. Single Element in a Sorted Array [Medium] (java)

---
+++ solutions/Medium/0540-single-element-in-a-sorted-array/solution.java
@@ -0,0 +1,56 @@
+// ──────────────────────────────────────────────────
+// Problem  : 540. Single Element in a Sorted Array
+// Difficulty: Medium
+// Tags     : Array, Binary Search
+// Link     : https://leetcode.com/problems/single-element-in-a-sorted-array/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42392000 (beats 0%)
+// Language : java
+// Copyright: (c) 2026 SnehaAnand21. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid == 0 && nums[0] != nums[1]) {
+                return nums[mid];
+            }
+            if (mid == n - 1 && nums[n - 1] != nums[n - 2]) {
+                return nums[mid];
+            }
+            if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+
+            if (mid % 2 == 0) {
+                if (nums[mid - 1] == nums[mid]) {
+                    end = mid - 1;
+                } 
+                else {
+                    start = mid + 1;
+                }
+            } 
+            else {
+                if (nums[mid - 1] == nums[mid]) {
+                    start = mid + 1;
+                } 
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
