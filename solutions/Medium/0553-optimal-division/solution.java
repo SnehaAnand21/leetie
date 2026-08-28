c44c35ab427bd45e757f83164a47dc6a65bd40ee
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-28 04:10:10 +0000

    leetie: Add 553. Optimal Division [Medium] (java)

--- solutions/Medium/0553-optimal-division/solution.java
+++ solutions/Medium/0553-optimal-division/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Math, Dynamic Programming
 // Link     : https://leetcode.com/problems/optimal-division/
-// Runtime  : 10 ms (beats 13%)
-// Memory   : 49432000 (beats 7%)
+// Runtime  : 11 ms (beats 8%)
+// Memory   : 49328000 (beats 10%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
