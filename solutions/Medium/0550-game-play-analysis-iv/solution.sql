3ea21dd140d40cdbc9076422d59c1cc3d3449d88
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-28 03:46:49 +0000

    leetie: Add 550. Game Play Analysis IV [Medium] (mysql)

---
+++ solutions/Medium/0550-game-play-analysis-iv/solution.sql
@@ -0,0 +1,33 @@
+-- ──────────────────────────────────────────────────
+-- Problem  : 550. Game Play Analysis IV
+-- Difficulty: Medium
+-- Tags     : Database
+-- Link     : https://leetcode.com/problems/game-play-analysis-iv/
+-- Runtime  : 481 ms (beats 100%)
+-- Memory   : 0B (beats 100%)
+-- Language : mysql
+-- Copyright: (c) 2026 SnehaAnand21. All rights reserved.
+-- Synced by: leetie
+-- ──────────────────────────────────────────────────
+
+WITH first_logins AS (
+  SELECT
+    A.player_id,
+    MIN(A.event_date) AS first_login
+  FROM
+    Activity A
+  GROUP BY
+    A.player_id
+), consec_logins AS (
+  SELECT
+    COUNT(A.player_id) AS num_logins
+  FROM
+    first_logins F
+    INNER JOIN Activity A ON F.player_id = A.player_id
+    AND F.first_login = DATE_SUB(A.event_date, INTERVAL 1 DAY)
+)
+SELECT
+  ROUND(
+    (SELECT C.num_logins FROM consec_logins C)
+    / (SELECT COUNT(F.player_id) FROM first_logins F)
+  , 2) AS fraction;
