f4df6a61ad32e3f2108900892df562f273ebefe1
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-28 16:54:55 +0000

    leetie: Add 586. Customer Placing the Largest Number of Orders [Easy] (mysql)

--- solutions/Easy/0586-customer-placing-the-largest-number-of-orders/solution.sql
+++ solutions/Easy/0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -3,7 +3,7 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
--- Runtime  : 642 ms (beats 15%)
+-- Runtime  : 510 ms (beats 53%)
 -- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 SnehaAnand21. All rights reserved.
