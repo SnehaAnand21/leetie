22c362f393bcec30f23e31c102f284af32f9c900
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-28 17:41:26 +0000

    leetie: Add 617. Merge Two Binary Trees [Easy] (java)

--- solutions/Easy/0617-merge-two-binary-trees/solution.java
+++ solutions/Easy/0617-merge-two-binary-trees/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Tree, Depth-First Search, Breadth-First Search, Binary Tree
 // Link     : https://leetcode.com/problems/merge-two-binary-trees/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42428000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 46828000 (beats 76%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
