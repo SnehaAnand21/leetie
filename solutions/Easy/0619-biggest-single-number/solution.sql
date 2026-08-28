2c1c06a21c5a9a0f4d1f2a3dce7be19c0e2192f0
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-28 17:42:27 +0000

    leetie: Add 619. Biggest Single Number [Easy] (mysql)

--- solutions/Easy/0619-biggest-single-number/solution.sql
+++ solutions/Easy/0619-biggest-single-number/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/biggest-single-number/
--- Runtime  : 108 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 504 ms (beats 35%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 -- Synced by: leetie
