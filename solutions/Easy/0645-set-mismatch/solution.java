a94f94ce6a9eca7b73e114e020dfbd497e2f610d
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-29 11:35:10 +0000

    leetie: Add 645. Set Mismatch [Easy] (java)

--- solutions/Easy/0645-set-mismatch/solution.java
+++ solutions/Easy/0645-set-mismatch/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, Bit Manipulation, Sorting
 // Link     : https://leetcode.com/problems/set-mismatch/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42908000 (beats 0%)
+// Runtime  : 762 ms (beats 5%)
+// Memory   : 47248000 (beats 94%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
