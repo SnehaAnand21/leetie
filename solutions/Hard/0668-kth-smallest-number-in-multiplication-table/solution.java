8455a5d2217061198676da1c60628f2210cf1a08
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-29 17:28:51 +0000

    leetie: Add 668. Kth Smallest Number in Multiplication Table [Hard] (java)

---
+++ solutions/Hard/0668-kth-smallest-number-in-multiplication-table/solution.java
@@ -0,0 +1,38 @@
+// ──────────────────────────────────────────────────
+// Problem  : 668. Kth Smallest Number in Multiplication Table
+// Difficulty: Hard
+// Tags     : Math, Binary Search
+// Link     : https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
+// Runtime  : 8 ms (beats 99%)
+// Memory   : 42348000 (beats 8%)
+// Language : java
+// Copyright: (c) 2026 SnehaAnand21. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        if (m > n) {
+            int temp = m;
+            m = n;
+            n = temp;
+        }
+        int left = 1;
+        int right = m*n;
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(enough(m,n,k,mid)) right = mid;
+            else left = mid+1;
+        }
+        return left;
+    }
+    private boolean enough(int m ,int n,int k , int num){
+        int count =0;
+        for(int i =1;i<=m;i++){
+            int add = Math.min(num/i , n);
+            if(add==0) break ;
+            count+=add;
+        }
+        return count >= k;
+    }
+}
