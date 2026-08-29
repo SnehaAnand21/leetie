5ee1297168ce123ac1905e39d6ceb91daa2f33e8
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-29 11:16:09 +0000

    leetie: Add 626. Exchange Seats [Medium] (mysql)

--- solutions/Medium/0626-exchange-seats/solution.sql
+++ solutions/Medium/0626-exchange-seats/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/exchange-seats/
--- Runtime  : 77 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 325 ms (beats 96%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 -- Synced by: leetie
