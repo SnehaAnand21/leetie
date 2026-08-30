31312ac11c1f9e038c75032e1f1973f642706480
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-30 17:11:11 +0000

    leetie: Add 704. Binary Search [Easy] (java)

--- solutions/Easy/0704-binary-search/solution.java
+++ solutions/Easy/0704-binary-search/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Binary Search
 // Link     : https://leetcode.com/problems/binary-search/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42616000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 48504000 (beats 14%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
