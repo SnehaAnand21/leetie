f63e7ec430cef4c1283b712e7898ed94e6b220d3
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-08-31 05:39:31 +0000

    leetie: Add 788. Rotated Digits [Medium] (java)

--- solutions/Medium/0788-rotated-digits/solution.java
+++ solutions/Medium/0788-rotated-digits/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Dynamic Programming
 // Link     : https://leetcode.com/problems/rotated-digits/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 41948000 (beats 0%)
+// Runtime  : 4 ms (beats 81%)
+// Memory   : 41840000 (beats 93%)
 // Language : java
 // Copyright: (c) 2026 SnehaAnand21. All rights reserved.
 // Synced by: leetie
