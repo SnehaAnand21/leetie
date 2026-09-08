163e6dabfb98bffc444af0ffae625e42b3cc2427
SnehaAnand21 <snehaanand21@users.noreply.github.com>
2026-09-08 06:01:02 +0000

    leetie: Add 3870. Count Commas in Range [Easy] (java)

---
+++ solutions/Easy/3870-count-commas-in-range/solution.java
@@ -0,0 +1,26 @@
+// ──────────────────────────────────────────────────
+// Problem  : 3870. Count Commas in Range
+// Difficulty: Easy
+// Tags     : Math
+// Link     : https://leetcode.com/problems/count-commas-in-range/
+// Runtime  : 1 ms (beats 84%)
+// Memory   : 42664000 (beats 40%)
+// Language : java
+// Copyright: (c) 2026 SnehaAnand21. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public int countCommas(int n) {
+        long answer = 0;
+        long threshold = 1000;
+
+        while (threshold <= n) {
+            answer += n - threshold + 1;
+
+            threshold *= 1000;
+        }
+
+        return (int) answer;
+    }
+}
